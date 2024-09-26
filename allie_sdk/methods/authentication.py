92a7d782839d21de3fe21b4fc864e16e93513fc7
Diethard Steiner <diethard.steiner@alation.com>
2024-09-26 16:08:11 +0100

    rework of error handling

--- allie_sdk/methods/authentication.py
+++ allie_sdk/methods/authentication.py
@@ -5,6 +5,7 @@ import requests
 
 from ..core.request_handler import RequestHandler
 from ..models.authentication_model import *
+from ..models.job_model import *
 
 LOGGER = logging.getLogger('allie_sdk_logger')
 
@@ -28,11 +29,12 @@ class AlationAuthentication(RequestHandler):
         self.refresh_token = refresh_token
         self.user_id = user_id
 
-    def validate_refresh_token(self) -> RefreshToken:
+    def validate_refresh_token(self) -> RefreshToken | JobDetails:
         """Validate the Alation API Refresh Token.
 
         Returns:
-            RefreshToken: Alation API Refresh Token.
+            RefreshToken: Alation API Refresh Token if request succeeded
+            JobDetails: Error details if request fails
 
         """
         validate_body = {
@@ -44,21 +46,40 @@ class AlationAuthentication(RequestHandler):
                              validate_body)
 
         if validity:
-            return RefreshToken.from_api_response(validity)
+            # cater for error messages
+            # check if the dict contains a status property
+            status = validity.get("status")
+            if status:
+                if status == "failed":
+                    return JobDetails.from_api_response(validity)
+            else:
+                return RefreshToken.from_api_response(validity)
 
-    def create_access_token(self) -> AccessToken:
+    def create_access_token(self) -> AccessToken | JobDetails:
         """Create an Alation API Access Token.
 
         Returns:
-            AccessToken: Alation API Access Token.
+            AccessToken: Alation API Access Token if request succeeded.
+            JobDetails: Error details if request failed.
 
         """
-        refresh_status = self.validate_refresh_token()
+        validate_refresh_token_response = self.validate_refresh_token()
 
-        if refresh_status.token_status.upper() != 'ACTIVE':
-            LOGGER.error("The Refresh Token is expired! Please generate a new"
-                         " refresh token and try again.")
+        # Case: Refresh token is expired
+        if isinstance(validate_refresh_token_response, RefreshToken):
+            if validate_refresh_token_response.token_status.upper() != 'ACTIVE':
+                validation_error_message = "The Refresh Token is expired! Please generate a new refresh token and try again."
+                LOGGER.error(validation_error_message)
 
+                # make it conform to JobDetails structure
+                mapped_response = self._map_request_error_to_job_details(validation_error_message)
+                return JobDetails.from_api_response(mapped_response)
+        # Case: There was some other error validating the refresh token
+        elif isinstance(validate_refresh_token_response, JobDetails):
+                # just pass on the error message
+                # it's already in the JobDetails structure
+                return validate_refresh_token_response
+        # Case: We've got a valid refresh token and can proceed with creating a new access token
         else:
             token_body = {'refresh_token': self.refresh_token,
                           'user_id': self.user_id}
@@ -66,27 +87,47 @@ class AlationAuthentication(RequestHandler):
                               token_body)
 
             if token:
-                return AccessToken.from_api_response(token)
+                # cater for error messages
+                # check if the dict contains a status property
+                status = token.get("status")
+                if status:
+                    if status == "failed":
+                        return JobDetails.from_api_response(token)
+                else:
+                    return AccessToken.from_api_response(token)
 
-    def validate_access_token(self, access_token: str) -> AccessToken:
+    def validate_access_token(self, access_token: str) -> AccessToken | JobDetails:
         """Validate the Alation API Access Token.
 
         Args:
             access_token (str): Alation API Access Token.
 
         Returns:
-            AccessToken: Alation API Access Token.
+            AccessToken: Alation API Access Token if request is successful
+            JobDetails: Error details if request fails
 
         """
-        validate_body = {'api_access_token': access_token,
-                         'user_id': self.user_id}
-        validity = self.post('/integration/v1/validateAPIAccessToken/',
-                             validate_body)
+        validate_body = {
+            'api_access_token': access_token
+            , 'user_id': self.user_id
+        }
+
+        validity = self.post(
+            url = '/integration/v1/validateAPIAccessToken/'
+            , body = validate_body
+        )
 
         if validity:
-            return AccessToken.from_api_response(validity)
+            # cater for error messages
+            # check if the dict contains a status property
+            status = validity.get("status")
+            if status:
+                if status == "failed":
+                    return JobDetails.from_api_response(validity)
+            else:
+                return AccessToken.from_api_response(validity)
 
-    def revoke_access_tokens(self) -> bool:
+    def revoke_access_tokens(self) -> JobDetails:
         """Revoke the Alation API Access Tokens of an Alation API Refresh Token.
 
         Returns:
@@ -95,7 +136,20 @@ class AlationAuthentication(RequestHandler):
         """
         revoke_body = {'refresh_token': self.refresh_token,
                        'user_id': self.user_id}
-        revoke = self.post('/integration/v1/revokeAPIAccessTokens/',
-                           revoke_body)
+        revoke_response = self.post(
+            url = '/integration/v1/revokeAPIAccessTokens/'
+            , body = revoke_body
+        )
 
-        return True if revoke else False
+
+        if revoke_response:
+            # check whether it was a success or failure
+            status = revoke_response.get("status")
+            if status:
+                if status == "failed":
+                    return JobDetails.from_api_response(revoke_response)
+            else:
+                mapped_revoke_response = self._map_request_success_to_job_details(revoke_response)
+                return JobDetails.from_api_response(mapped_revoke_response)
+
+
