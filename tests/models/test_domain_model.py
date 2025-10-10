5974fa6239aef286725fa4cbc1013fa21ddaad50
Diethard Steiner <82887990+diethard-at-alation@users.noreply.github.com>
2025-10-10 16:15:06 +0100

    Add domain membership rule viewing support (#87)
    * Add domain membership rule viewing support
    
    * minor adjustments

--- tests/models/test_domain_model.py
+++ tests/models/test_domain_model.py
@@ -45,3 +45,41 @@ class TestDomainModels(unittest.TestCase):
         }
 
         self.assertEqual(input_transformed, output)
+
+    def test_domain_membership_rule_request_payload(self):
+        request = DomainMembershipRuleRequest(
+            domain_ids=[1, 2],
+            exclude=True,
+            recursive=False,
+        )
+
+        self.assertEqual(
+            request.generate_api_post_payload(),
+            {
+                "domain_id": [1, 2],
+                "exclude": True,
+                "recursive": False,
+            },
+        )
+
+    def test_domain_membership_rule_from_response(self):
+        rule = DomainMembershipRule.from_api_response(
+            {
+                "domain_id": 1,
+                "exclude": False,
+                "recursive": True,
+                "otype": "table",
+                "oid": 42,
+            }
+        )
+
+        self.assertEqual(
+            rule,
+            DomainMembershipRule(
+                domain_id=1,
+                exclude=False,
+                recursive=True,
+                otype="table",
+                oid=42,
+            ),
+        )
